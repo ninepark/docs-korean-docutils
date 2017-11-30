3d51f8ff0fb2496a77c5245dfcc65981c1b32a0d
joelkim <kim.dohhyoung@gmail.com>
2017-11-30 12:23:27 +0900

    edit

--- create_ghpages.sh
+++ create_ghpages.sh
@@ -1,15 +1,21 @@
 # current git branch must be `docs-korean`
 git add . -A
 git commit -m "edit"
+git push origin docs-korean
+
 git checkout -B gh-pages
 git rebase docs-korean
+
 touch .nojekyll
+
 cd docutils
 tools/buildhtml.py --config=tools/docutils.conf
+
 cd ..
 git add . -A
 git commit -m "build"
 git push -f origin gh-pages
 git reset --hard HEAD
 git clean -f
+
 git checkout docs-korean
